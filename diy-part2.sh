10ec9812a4ea78ecd0270c90e1b186f18c4778a6
ericbest2022 <wenlong2022@qq.com>
2026-06-16 19:57:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,7 @@ git clone https://github.com/theosoft-git/luci-app-easymesh.git package/luci-app
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify default IP
-# sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.*.1/192.168.166.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 sed -i 's/LEDE/iStoreOS/g' package/base-files/files/bin/config_generate
